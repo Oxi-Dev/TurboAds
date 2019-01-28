a1a7634d4afbda58337bd3f2b7b16a3f39720458
Gabriela Dan <gabriela.dan@bioelite.ro>
2019-01-28 19:44:36 +0000

    New feature: Activity Page

--- app/channels/private/conversation_channel.rb
+++ app/channels/private/conversation_channel.rb
@@ -14,6 +14,20 @@ class Private::ConversationChannel < ApplicationCable::Channel
       hash[el['name']] = el['value']
     end
     Private::Message.create(message_params)
+            
+    #track this on activities table
+    conversation = Private::Conversation.where(id: message_params['conversation_id']).first   
+    @is_user_sender = message_params['user_id'].to_s == conversation.sender_id.to_s
+    
+    if @is_user_sender
+        userDetails = User.where(id: conversation.recipient_id).first 
+    else
+        userDetails = User.where(id: conversation.sender_id).first      
+    end    
+    
+    activity_description = 'New message between '+ current_user.name + ' - ' +  userDetails.name
+        
+    Activity.create(name: "New message", description: activity_description, activity_type: 'message')
   end
   
   def set_as_seen(data)
